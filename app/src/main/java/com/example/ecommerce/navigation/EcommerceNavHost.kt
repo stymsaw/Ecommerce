1a8810ec9c8d556ce166c5748af3bb6befcfc7eb
Satyam Saw <satyamatibarts@gmail.com>
2024-01-11 00:12:31 +0530

    Added View Model & TabRows

--- app/src/main/java/com/example/ecommerce/navigation/EcommerceNavHost.kt
+++ app/src/main/java/com/example/ecommerce/navigation/EcommerceNavHost.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.ecommerce.ui.screens.HomeScreen
+import com.example.ecommerce.ui.homescreen.HomeScreen
 import com.example.ecommerce.ui.splashscreen.SplashScreen
 
 @Composable
--- app/src/main/java/com/example/ecommerce/ui/MainActivity.kt
+++ app/src/main/java/com/example/ecommerce/ui/MainActivity.kt
@@ -6,14 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.ecommerce.navigation.EcommerceNavHost
-import com.example.ecommerce.ui.screens.HomeScreen
-import com.example.ecommerce.ui.splashscreen.SplashScreen
 import com.example.ecommerce.ui.theme.EcommerceTheme
 
 class MainActivity : ComponentActivity() {
