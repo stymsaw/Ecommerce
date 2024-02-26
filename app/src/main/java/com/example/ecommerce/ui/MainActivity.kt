ade04592bf289f0a6bc80797a6d6869885e5a8b8
Satyam Saw <satyamkumarsaw30@gmail.com>
2024-02-26 19:42:38 +0530

    Added exit transition in nav host

--- app/src/main/java/com/example/ecommerce/ui/MainActivity.kt
+++ app/src/main/java/com/example/ecommerce/ui/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity() : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
+//  View model
         val mainVM = ViewModelProvider(this)[MainVM::class.java]
 
 
